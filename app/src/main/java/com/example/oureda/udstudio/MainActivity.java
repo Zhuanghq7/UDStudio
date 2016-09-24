d051952a514448825add4fdd36764a7b21381d48
Zhuangh7 <zhuanghq2007@foxmail.com>
2016-09-24 15:37:06 +0800

    增加了字符串解析及动画效果

--- app/src/main/java/com/example/oureda/udstudio/MainActivity.java
+++ app/src/main/java/com/example/oureda/udstudio/MainActivity.java
@@ -7,10 +7,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,9 +25,10 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends Activity {
-
+    private boolean firstUpdated = true;
     private ImageView topBar;
     private TextView textView;
+    public static int updateSuccessful = 0;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -41,6 +46,17 @@ public class MainActivity extends Activity {
         return false;
     }
 
+    public void createToast(String s){
+        Looper.prepare();
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        Looper.loop();
+    }
+    public void moveViews(){
+        if(firstUpdated){
+            removeTopBar();//把topbar移掉(下面的按钮会有闪现的效果=- =干脆删除吧
+            textView.setVisibility(View.GONE);//
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,10 +84,20 @@ public class MainActivity extends Activity {
                 //Toast.makeText(getApplicationContext(),"23333",Toast.LENGTH_LONG).show();
                 Log.d("log", "click");
                 if(checkNetworkInfo()){
-                    new freshFromServer().start();
+                    new freshFromServer(MainActivity.this).start();
+                    while(updateSuccessful == 0){
+
+                    }
+                    if(updateSuccessful == 1){
+                        Toast.makeText(getApplicationContext(),"update successful", Toast.LENGTH_SHORT).show();
+                        moveViews();
+                    }else{
+                        Toast.makeText(getApplicationContext(),"update failed",Toast.LENGTH_SHORT).show();
+                    }
+
                 }else{
                     Log.d("log","no internet connected");
-                    Toast.makeText(getApplicationContext(), "please check your net", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "please check your net connect", Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -83,6 +109,59 @@ public class MainActivity extends Activity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    public static void addViews(String s){
+        //根据服务器数据动态创建
+        String name = null;
+        int funNum = -1;
+        String[] funs;
+        int funsI = 0;
+        for(int i = 0;i<s.length();i++){
+            if(name == null && s.charAt(i) == ';') {
+                name = s.substring(0, i);
+                Log.d("log","getName:"+name);
+                s=s.substring(i+1);
+                break;
+            }
+        }
+
+        for(int i = 0;i<s.length();i++) {
+            if(name!=null&&funNum==-1&&s.charAt(i)==';') {
+                funNum = Integer.parseInt(s.substring(0, i));
+                Log.d("log", "funNum:" + funNum);
+                s = s.substring(i + 1);
+                break;
+            }
+        }
+
+        funs = new String[funNum];
+        int oldI = 0;
+        for(int i = 0;i<s.length();i++) {
+            if(s.charAt(i)==';'){
+                funs[funsI++] = s.substring(oldI,i);
+                oldI = i+1;
+
+                Log.d("log","fun "+(funsI)+" :"+funs[funsI-1]);
+            }
+        }
+
+        funs[funs.length-1] = s.substring(oldI);
+        Log.d("log","the last fun is : "+funs[funs.length-1]);
+    }
+    private void removeTopBar(){
+        AnimationSet animationSet = new AnimationSet(true);
+        TranslateAnimation translateAnimation = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF,0f,
+                Animation.RELATIVE_TO_SELF,0f,
+                Animation.RELATIVE_TO_SELF,0f,
+                Animation.RELATIVE_TO_SELF,-1f
+        );
+
+        translateAnimation.setDuration(1000);
+        animationSet.addAnimation(translateAnimation);
+        topBar.startAnimation(animationSet);
+        topBar.setVisibility(View.GONE);
+        firstUpdated = false;
+    }
     @Override
     public void onStart() {
         super.onStart();
--- app/src/main/java/com/example/oureda/udstudio/classes/freshFromServer.java
+++ app/src/main/java/com/example/oureda/udstudio/classes/freshFromServer.java
@@ -1,7 +1,10 @@
 package com.example.oureda.udstudio.classes;
 
+import android.app.Activity;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.example.oureda.udstudio.MainActivity;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,17 +15,31 @@ import java.net.Socket;
  */
 public class freshFromServer extends Thread{
 
+    private MainActivity activity;
+
+    private void log(String s){
+        Log.d("log",s);
+    }
+    public freshFromServer(MainActivity act){
+        activity = act;
+    }
 
     @Override
     public void run() {
         try {
             Socket s = new Socket("123.206.208.46", 7788);
             BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            String temp = br.readLine();
-            Log.d("log",temp);
-            //Toast.makeText(getApplicationContext(),temp,Toast.LENGTH_SHORT).show();
+            String temp = br.readLine();//得到控件数量
+            log("控件数量 "+temp);
+            int num = Integer.parseInt(temp);
+            for(int i = 0;i<num;i++) {
+                MainActivity.addViews(br.readLine());
+            }
+            activity.updateSuccessful = 1;
         } catch (Exception e) {
             Log.d("log","connect server failed");
+            activity.createToast("fail to connect server");
+            activity.updateSuccessful = 2;
             e.printStackTrace();
         }
     }
